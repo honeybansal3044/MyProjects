6342e6cbd81437ce6c584b0b5b8cc46180a5c86d
Honey Bansal <honey.bansal@spacetimeinsight.com>
2016-11-09 10:15:09 -0600

    Added file example

---
+++ com.nov.test/src/com/nov/test/Test/Test.java
@@ -0,0 +1,71 @@
+package com.nov.test.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * @author Honey Bansal
+ */
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+		main2();
+
+	}
+	
+	public static void main1() throws IOException {
+		FileInputStream in = null;
+		FileOutputStream out = null;
+
+		try {
+			in = new FileInputStream("input.txt");
+			out = new FileOutputStream("output.txt");
+
+			int c;
+			while((c = in.read()) != -1) {
+				out.write(c);
+			}
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(in != null) {
+					in.close();
+				}
+				if(out != null) {
+					out.close();
+				}
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void main2() throws IOException {
+	      FileReader in = null;
+	      FileWriter out = null;
+	      try {
+	         in = new FileReader("input.txt");
+	         out = new FileWriter("output.txt");
+	         
+	         int c;
+	         while ((c = in.read()) != -1) {
+	            out.write(c);
+	         }
+	      }finally {
+	         if (in != null) {
+	            in.close();
+	         }
+	         if (out != null) {
+	            out.close();
+	         }
+	      }
+	}
+
+}
