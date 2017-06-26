c289014ac5f03981a9f0495010411c67c7077b74
888600 <888600@WHQ-888600-W1.corp.ds.fedex.com>
2017-06-26 16:46:43 -0500

    Adding Required annotation in circle class.

--- spring/src/main/java/com/edu/spring/Circle.java
+++ spring/src/main/java/com/edu/spring/Circle.java
@@ -1,5 +1,7 @@
 package com.edu.spring;
 
+import org.springframework.beans.factory.annotation.Required;
+
 public class Circle implements Shape {
 
 	private Point center;
@@ -7,7 +9,14 @@ public class Circle implements Shape {
 	public Point getCenter() {
 		return center;
 	}
-
+	/*
+	 * @Required annotation is used if we want this property required. It means bean will not be created 
+	 * by IOC container if this property is not injected to this bean.
+	 * So if I do not pass <property> param in the bean def, bean will not be created as center is required in order to create this bean
+	 * we would need to configure following class in beans.xml in order to make it work. 
+	 * org.springframework.beans.factory.annotation.RequiredAnnotationBeanPostProcessor
+	 */
+	@Required
 	public void setCenter(Point center) {
 		this.center = center;
 	}
