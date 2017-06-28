290dbb266698f9398f4ecdc5ca2e37fd39953970
888600 <888600@WHQ-888600-W1.corp.ds.fedex.com>
2017-06-28 14:49:31 -0500

    Committing changes for JSR-250 annotations, Message Bundling and Event Handling.

--- spring/src/main/java/com/edu/spring/Circle.java
+++ spring/src/main/java/com/edu/spring/Circle.java
@@ -3,10 +3,17 @@ package com.edu.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.context.MessageSource;
 
-public class Circle implements Shape {
+import com.edu.eventhandling.DrawEvent;
+
+public class Circle implements Shape, ApplicationEventPublisherAware {
 
 	private Point center;
+	private MessageSource messageSource;
+	private ApplicationEventPublisher publisher;
 	
 	public Point getCenter() {
 		return center;
@@ -27,13 +34,34 @@ public class Circle implements Shape {
 	@Required
 	@Autowired
 	@Qualifier("pointA")
+	/*@Qualifier("pointAA")*/
 	public void setCenter(Point center) {
 		this.center = center;
 	}
 
+	
+	
+	public MessageSource getMessageSource() {
+		return messageSource;
+	}
+
+	@Autowired
+	public void setMessageSource(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+
 	public void draw() {
-		System.out.println("Circle Point is ( " +center.getX() +"," +center.getY() +")");
-		
+		System.out.println(this.getMessageSource().getMessage("drawing.circle", null, "default greeting", null));
+		System.out.println(this.getMessageSource().getMessage("drawing.point", new Object[] {center.getX(), center.getY()}, "default greeting", null));
+		DrawEvent drawEvenet = new DrawEvent(this);
+		publisher.publishEvent(drawEvenet.toString());
+		//System.out.println("Circle Point is ( " +center.getX() +"," +center.getY() +")");
+		//System.out.println(this.getMessageSource().getMessage("greeting", null, "default greeting", null));
+	}
+
+	@Override
+	public void setApplicationEventPublisher(ApplicationEventPublisher publisher) {
+		this.publisher = publisher;
 	}
 
 }
