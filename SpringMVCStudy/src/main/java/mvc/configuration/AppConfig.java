51ac2d0725e09eb46758df623b7519d6278acf05
Honey Bansal <honeybansal3044@gmail.com>
2017-06-29 17:47:20 -0500

    updated

---
+++ SpringMVCStudy/src/main/java/mvc/configuration/AppConfig.java
@@ -0,0 +1,9 @@
+package mvc.configuration;
+
+/*import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
+*/
--- SpringMVCStudy/src/main/java/mvc/configuration/MyWebApplicationInitializer.java
+++ SpringMVCStudy/src/main/java/mvc/configuration/MyWebApplicationInitializer.java
@@ -1,33 +1,19 @@
 package mvc.configuration;
 
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+/*import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
-/*public class MyWebApplicationInitializer implements WebApplicationInitializer {
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MyWebApplicationInitializer implements WebApplicationInitializer {
 
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		ServletRegistration.Dynamic reg = servletContext.addServlet("SpringMVCStudy", new DispatcherServlet());
 		reg.setLoadOnStartup(1);
-		reg.addMapping("/SpringMVCStudy/*.htm");
-	}
-
-}*/
-
-/*public class MyWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-
-	@Override
-	protected Class<?>[] getRootConfigClasses() {
-		return null;
-	}
-
-	@Override
-	protected Class<?>[] getServletConfigClasses() {
-		return null;
-	}
-
-	@Override
-	protected String[] getServletMappings() {
-		return null;
+		reg.addMapping("/*.htm");
 	}
 
 }*/
