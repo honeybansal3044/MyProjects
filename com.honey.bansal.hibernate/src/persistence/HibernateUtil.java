acab0df5c6f34ddce7e82b941868d7a1cf706561
927456 <927456@WHQ-781442-W1.corp.ds.fedex.com>
2019-01-25 11:22:08 -0600

    changes for postgres

--- com.honey.bansal.hibernate/src/persistence/HibernateUtil.java
+++ com.honey.bansal.hibernate/src/persistence/HibernateUtil.java
@@ -10,11 +10,13 @@ public class HibernateUtil {
 
   private static SessionFactory sessionFactory;
   private static SessionFactory sessionFactoryMySql;
+  private static SessionFactory sessionFactoryPostgres;
 
   static {
     try {
        sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
        sessionFactoryMySql = new Configuration().configure("hibernate.mysql.cfg.xml").buildSessionFactory();
+       sessionFactoryPostgres = new Configuration().configure("hibernate.postgres.cfg.xml").buildSessionFactory();
     } catch (Throwable ex) {
        throw new ExceptionInInitializerError(ex);
     }
@@ -29,10 +31,16 @@ public class HibernateUtil {
       // Alternatively, we could look up in JNDI here
       return sessionFactoryMySql;
   }
+  
+  public static SessionFactory getSessionFactoryPostgres() {
+      // Alternatively, we could look up in JNDI here
+      return sessionFactoryPostgres;
+  }
 
   public static void shutdown() {
       // Close caches and connection pools
       getSessionFactory().close();
       getSessionFactoryMySql().close();
+      getSessionFactoryPostgres().close();
   }
 }
